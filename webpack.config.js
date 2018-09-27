db6947ff556a5b8e4fe7f35901873e57e0048fa7
chandu <chandrasr.palle@gmail.com>
2018-09-27 15:27:59 +0200

    plugin added for html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry : './src/js/index.js',
     output : {
@@ -6,5 +8,10 @@ module.exports = {
         filename: 'bundle.js'
     },devServer : {
         contentBase : './dist'
-    }
+    },plugins :[
+        new htmlWebpackPlugin({
+            filename : 'index.html',
+            template : './src/index.html'            
+        })
+    ]
 };
