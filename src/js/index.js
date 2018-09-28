11986ccbd0ffcc5d560d5b1d46fd27b5b0f97ab9
chandu <chandrasr.palle@gmail.com>
2018-09-28 12:54:02 +0200

    event listner added for Search form

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,34 @@
 import Search from './models/Search';
 
-const search = new Search("Pizza");
-search.getResutls();
+/**Global state of the app
+ * Search object
+ * Current recipe object
+ * shopping list object
+ * liked recipes
+ */
+const state = {
+
+};
+const controlSearch = async ()=>{
+    // Get query from the view
+    const query = "Pizza";
+    if(query){
+        // Perform Search with the query by creating new Search object
+        state.search = new Search(query);
+
+        // Prepare UI for results
 
 
+        // Search for recipes
+        await state.search.getResutls();
+
+        // Render results on UI
+        console.log(state.search.result);
+    }
+
+
+};
+document.querySelector(".search").addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+});
