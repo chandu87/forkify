7bf85ddbed8824608f6cc452046926ed3a6380bc
chandu <chandrasr.palle@gmail.com>
2018-09-28 15:16:27 +0200

    building serachView for retrive and display input from UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
-
+import {elements} from './views/base';
+import * as searchView from './views/searchView';
 /**Global state of the app
  * Search object
  * Current recipe object
@@ -11,24 +12,27 @@ const state = {
 };
 const controlSearch = async ()=>{
     // Get query from the view
-    const query = "Pizza";
+    const query = searchView.getInput();
+    console.log(query);
     if(query){
         // Perform Search with the query by creating new Search object
         state.search = new Search(query);
 
         // Prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
 
         // Search for recipes
         await state.search.getResutls();
 
         // Render results on UI
         console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 
 
 };
-document.querySelector(".search").addEventListener('submit', e =>{
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     controlSearch();
 });
