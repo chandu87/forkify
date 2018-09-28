464c00f9562680b7948a313310ab075e17caf81a
chandu <chandrasr.palle@gmail.com>
2018-09-28 11:55:05 +0200

    search model added

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,6 @@
-import axios from "axios";
+import Search from './models/Search';
 
-async function searchFoodForkApi(query) {
-  const apiKey = "66bdd2ec39d9591c3a9a10b507f104b9";
-  try {
-    const res = await axios(
-      `https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-searchFoodForkApi("pizza");
+const search = new Search("Pizza");
+search.getResutls();
+
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+class Search{
+    constructor(query){
+        this.query = query;
+    }
+    async getResutls() {
+        const apiKey = "66bdd2ec39d9591c3a9a10b507f104b9";
+        try {
+          const res = await axios(
+            `https://www.food2fork.com/api/search?key=${apiKey}&q=${this.query}`
+          );
+          this.result = res.data.recipes;
+        //   console.log(this.result);
+        } catch (error) {
+          alert(error);
+        }
+      }
+}
+
+export default Search;
