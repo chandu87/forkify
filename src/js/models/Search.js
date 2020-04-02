24b3047b583bad0101003be1fad23f6f9792b48c
chandu <chandrasr.palle@gmail.com>
2020-04-02 17:29:30 +0200

    api url changed

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,7 +8,7 @@ class Search{
         const apiKey = "66bdd2ec39d9591c3a9a10b507f104b9";
         try {
           const res = await axios(
-            `https://www.food2fork.com/api/search?key=${apiKey}&q=${this.query}`
+            `https://forkify-api.herokuapp.com/api/search?q=${this.query}`
           );
           this.result = res.data.recipes;
         //   console.log(this.result);
